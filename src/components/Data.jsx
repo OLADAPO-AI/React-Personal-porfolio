b1eba0ce9f7d3e0c3e20b89fe6be390f6a1b58c9
Oladapo Falusi <falusioladapo@gmail.com>
2023-07-24 09:50:06 +0100

    second commit

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -3,28 +3,28 @@
 export default [
     {
         id : 1,
-        img : "./src/images/flash.jpg",
+        img : "/flash.jpg",
         demo : "",
         code : ""
     },
 
     {
         id : 2,
-        img : "./src/images/flash.jpg",
+        img : "/flash.jpg",
         demo : "",
         code : ""
     },
 
     {
         id : 3,
-        img : "./src/images/project2.jpg",
+        img : "/project2.jpg",
         demo : "",
         code : ""
     },
 
     {
         id : 4,
-        img : "./src/images/project1.jpg",
+        img : "/project1.jpg",
         demo : "https://professional-template-beta.vercel.app/",
         code : "https://github.com/OLADAPO-AI/Professional-Template",
         h3 : "A Tailwind/Js Professional Website"
@@ -32,7 +32,7 @@ export default [
 
     { 
         id : 5,
-        img : "./src/images/flash.jpg",
+        img : "/flash.jpg",
         demo : "",
         code : ""
     },
